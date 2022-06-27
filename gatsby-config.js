805c9a4e97c5a06d081037393ab49c673248b040
emanuele-em <micheletti.emanuele@hotmail.com>
2022-06-27 11:34:01 +0200

    finished part 4 gatsby tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title: "Title Test",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
